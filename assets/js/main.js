2500fecfa55f792b4fbca38468d00fc5dac97cd5
anhnnv137 <nguyenngovietanh19@gmail.com>
2022-11-30 11:31:11 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let introduceArrow = document.querySelector(".introduce-arrow");
+introduceArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jobArrow = document.querySelector(".job-arrow");
+jobArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
+let guideArrow = document.querySelector(".guide-arrow");
+guideArrow.onclick = function() {
+ navLinks.classList.toggle("show4");
+}
+let teacherArrow = document.querySelector(".teacher-arrow");
+teacherArrow.onclick = function() {
+ navLinks.classList.toggle("show5");
+}
